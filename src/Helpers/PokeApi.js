1b206a10a0af387e9a87d1c611c5e4966d23d692
sucitanjung2798 <sucitanjung2798@gmail.com>
2023-11-25 18:06:56 +0700

    add API to get AllPokemonData and get Detail Data

---
+++ src/Helpers/PokeApi.js
@@ -0,0 +1,44 @@
+const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+
+export const Colors = {
+  grass: "#87c75e",
+  poison: "#b84fb9",
+  fire: "#eb7471",
+  flying: "#7dd3fc",
+  water: "#8ac5f8",
+  bug: "#70ccb0",
+  normal: "#d4d4d4",
+  electric: "#ffcf4a",
+  ground: "#e1c169",
+  fairy: "#FAD9E6",
+  fighting: "#c03028",
+  psychic: "#f85888",
+  rock: "#b7a038",
+  steel: "#b8b8d0",
+  ice: "#98d9d9",
+  ghost: "#715796",
+  dragon: "#7138f8",
+  dark: "#6e5849",
+  unknown: "FFFDFDFD",
+};
+
+export const getPokemons = async (limit = 20, offset = 0) => {
+  try {
+    let url = `${baseUrl}?limit=${limit}&offset=${offset}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getPokemonData = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
