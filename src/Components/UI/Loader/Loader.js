7a6154b82eaba0e95d92c0cd92b404e6847d360b
sucitanjung2798 <sucitanjung2798@gmail.com>
2023-11-25 18:08:27 +0700

    add Loader file

---
+++ src/Components/UI/Loader/Loader.js
@@ -0,0 +1,13 @@
+import PikaLoader from "../../../Assets/Pika_loader.gif";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <figure className="loader">
+      <img className="scale-150" src={PikaLoader} alt="Running Pikachu" />
+      <figcaption className="pt-4 ml-4 text-xl">Loading...</figcaption>
+    </figure>
+  );
+};
+
+export default Loader;
